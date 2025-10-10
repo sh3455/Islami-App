567ba09c2cc10112e2f97ba0878d4fc24e7ff634
Qaiaty-Store <shahdsalah573@gmail.com>
2025-10-10 17:11:31 +0300

    Initial project. Added empty home screen.

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  static const String routeName = "HomeScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
